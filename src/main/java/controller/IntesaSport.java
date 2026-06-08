f92b60daa9acbb0fc088ccca26b0212cee65a0a5
Angela Del Prete <angeladelprete84@gmail.com>
2026-06-08 21:42:57 +0200

    aggEsAggiornato

--- src/main/java/controller/IntesaSport.java
+++ src/main/java/controller/IntesaSport.java
@@ -2,6 +2,7 @@ package controller;
 
 import entity.*;
 
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
@@ -71,5 +72,47 @@ public class IntesaSport {
         }
 
         return righeTabella;
+    }
+    public Esercizio creaEsercizio(String nome, String descrizione) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome esercizio non valido");
+        }
+
+        Esercizio esercizio = new Esercizio();
+        esercizio.setNome(nome);
+        esercizio.setDescrizione(descrizione);
+
+        return esercizio;
+    }
+
+    public void aggiungiEsercizio(
+            SessioneAllenamento sessione,
+            Esercizio esercizio,
+            Integer ripetizioniPreviste,
+            Duration durataPrevista
+    ) {
+        if (sessione == null) {
+            throw new IllegalArgumentException("Sessione non valida");
+        }
+
+        if (esercizio == null) {
+            throw new IllegalArgumentException("Esercizio non valido");
+        }
+
+        if (ripetizioniPreviste == null && durataPrevista == null) {
+            throw new IllegalArgumentException("Inserire ripetizioni o durata");
+        }
+
+        DettaglioEsercizio dettaglio = new DettaglioEsercizio();
+        dettaglio.setSessioneAllenamento(sessione);
+        dettaglio.setEsercizio(esercizio);
+
+        if (ripetizioniPreviste != null) {
+            dettaglio.setRipetizioni(ripetizioniPreviste);
+        }
+
+        dettaglio.setDurata(durataPrevista);
+
+        sessione.aggiungiDettaglioEsercizio(dettaglio);
     }
 }
